40924cfa3c5e956b8af0e0eee60c5ae328050d52
laizhebusan1987 <450425075@qq.com>
2019-06-01 12:36:53 +0800

    提交订单模块

---
+++ huinong/e-commerce-order/src/main/java/org/huinong/ecommerceorder/ECommerceOrderApplication.java
@@ -0,0 +1,13 @@
+package org.huinong.ecommerceorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceOrderApplication.class, args);
+	}
+
+}
